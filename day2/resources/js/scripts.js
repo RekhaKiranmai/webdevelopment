886c78c951384316a65d90e64ba921f13d79003d
RekhaKiranmai <60733859+RekhaKiranmai@users.noreply.github.com>
2020-07-07 13:28:14 +0530

    d2

---
+++ day2/resources/js/scripts.js
@@ -0,0 +1,139 @@
+var names = ['Assurance', 'Safety', 'Comfort', 'Wellness','Risk Management' , 'Employees'];
+
+    setInterval(function() {
+      var rand = Math.floor(Math.random() * 4);
+      document.getElementById("name").innerHTML = names[rand];
+}, 2000);
+  	
+  		$(document).ready(function() {
+  		var oldSize = parseFloat($("#zoom").css('font-size'));
+  		var newSize = oldSize  * 1.2;
+  		$("#zoom").hover(
+    	function() {
+     	$("#zoom").animate({ fontSize: newSize}, 100);
+    	},
+    	function() {
+    	$("#zoom").animate({ fontSize: oldSize}, 100);
+   		}
+ 		);
+		});
+		$(document).ready(function() {
+  		var oldSize = parseFloat($("#zooms").css('font-size'));
+  		var newSize = oldSize  * 1.2;
+  		$("#zooms").hover(
+    	function() {
+     	$("#zooms").animate({ fontSize: newSize}, 100);
+    	},
+    	function() {
+    	$("#zooms").animate({ fontSize: oldSize}, 100);
+   		}
+ 		);
+		});
+		$(document).ready(function() {
+  		var oldSize = parseFloat($("#izoom").css('font-size'));
+  		var newSize = oldSize  * 1.2;
+  		$("#izoom").hover(
+    	function() {
+     	$("#izoom").animate({ fontSize: newSize}, 100);
+    	},
+    	function() {
+    	$("#izoom").animate({ fontSize: oldSize}, 100);
+   		}
+ 		);
+		});
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom1").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom1").hover(
+      function() {
+      $("#zoom1").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom1").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom2").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom2").hover(
+      function() {
+      $("#zoom2").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom2").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom3").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom3").hover(
+      function() {
+      $("#zoom3").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom3").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom4").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom4").hover(
+      function() {
+      $("#zoom4").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom4").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom5").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom5").hover(
+      function() {
+      $("#zoom5").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom5").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom6").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom6").hover(
+      function() {
+      $("#zoom6").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom6").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom7").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom7").hover(
+      function() {
+      $("#zoom7").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom7").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
+    $(document).ready(function() {
+      var oldSize = parseFloat($("#zoom8").css('font-size'));
+      var newSize = oldSize  * 1.2;
+      $("#zoom8").hover(
+      function() {
+      $("#zoom8").animate({ fontSize: newSize}, 100);
+      },
+      function() {
+      $("#zoom8").animate({ fontSize: oldSize}, 100);
+      }
+    );
+    });
